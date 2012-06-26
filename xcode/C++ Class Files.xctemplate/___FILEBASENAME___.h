a1bc99369213636332136c09e701f3b3979e6085
Jean-Francois Dupuis <jeanfrancois.dupuis@botndolly.com>
2012-06-26 09:13:54 -0700

    Adding xcode templates for C++ class and Qt Widget

---
+++ xcode/C++ Class Files.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,20 @@
+/**
+ *  ___FILENAME___
+ *  ___PROJECTNAME___
+ *
+ *  Created by ___FULLUSERNAME___ on ___DATE___.
+ *  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+*/
+
+#ifndef ___PROJECTNAMEASIDENTIFIER_______FILEBASENAMEASIDENTIFIER_______FILEEXTENSION___
+#define ___PROJECTNAMEASIDENTIFIER_______FILEBASENAMEASIDENTIFIER_______FILEEXTENSION___
+
+class ___FILEBASENAMEASIDENTIFIER___ {
+private:
+    
+public:
+    ___FILEBASENAMEASIDENTIFIER___();
+    ~___FILEBASENAMEASIDENTIFIER___();
+};
+
+#endif
---
+++ xcode/QtWidget.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,29 @@
+/**
+ *  ___FILENAME___
+ *  ___PROJECTNAME___
+ *
+ *  Created by ___FULLUSERNAME___ on ___DATE___.
+ *  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+*/
+
+#ifndef ___PROJECTNAMEASIDENTIFIER_______FILEBASENAMEASIDENTIFIER_______FILEEXTENSION___
+#define ___PROJECTNAMEASIDENTIFIER_______FILEBASENAMEASIDENTIFIER_______FILEEXTENSION___
+
+#include <QWidget>
+
+class ___FILEBASENAMEASIDENTIFIER___ : public QWidget {
+	Q_OBJECT
+private:
+    
+signals:	
+    
+public slots:
+
+public:
+    ___FILEBASENAMEASIDENTIFIER___(QWidget *inParent = NULL);
+    ~___FILEBASENAMEASIDENTIFIER___();
+    
+
+};
+
+#endif
